c5684cae88f23007b5f25a4f2a5e56ced74f07dc
mahlonduke <53539373+mahlonduke@users.noreply.github.com>
2019-11-25 18:13:52 -0800

    Created initial folder structure and files

---
+++ Belly_Button_Diversity/models.py
@@ -0,0 +1,16 @@
+from .app import db
+
+######################
+# Example from a previous exercise:
+######################
+
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    lat = db.Column(db.Float)
+    lon = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Pet %r>' % (self.name)
